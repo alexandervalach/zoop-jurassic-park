f6da7b8b4a56bb1ec56d3aa0095fc681a75a7c12
Alexander Valach <alexander.valach@gmail.com>
2020-11-18 18:28:57 +0100

    Lesson 7 - Abtract method and classes

---
+++ src/07/src/attraction/Exhibition.java
@@ -0,0 +1,51 @@
+package attraction;
+
+import dinosaur.*;
+
+import java.util.ArrayList;
+
+public class Exhibition {
+
+  protected ArrayList<Herbivore> herbivores;
+  protected ArrayList<Carnivore> carnivores;
+
+  public Exhibition () {
+    herbivores = new ArrayList<>();
+    carnivores = new ArrayList<>();
+  }
+
+  public void seeDinos () {
+    herbivores.add(new Stegosaur());
+    carnivores.add(new Allosaur());
+    herbivores.add(new Triceratops());
+
+    printDinos();
+
+    // dinosaurs.set(1, dinosaurs.get(0));
+    /*
+    Dinosaur dinosaur0 = new Dinosaur();
+    Dinosaur dinosaur1 = new Dinosaur();
+
+    System.out.println(dinosaur0);
+    System.out.println(dinosaur1);
+
+    dinosaur1 = dinosaur0;
+    System.out.println(dinosaur0);
+    System.out.println(dinosaur1);
+
+    dinosaur1.setState(dinosaur0.getState() + " is sleepy");
+    System.out.println(dinosaur0.getState());
+    System.out.println(dinosaur1.getState());
+     */
+  }
+
+  public void printDinos () {
+    for (Herbivore herbivore : herbivores) {
+      System.out.println(herbivore);
+    }
+
+    for (Carnivore carnivore : carnivores) {
+      System.out.println(carnivore);
+    }
+  }
+}
---
+++ src/07/src/people/Visitor.java
@@ -0,0 +1,9 @@
+package people;
+
+public class Visitor {
+  private double money;
+
+  public void eat (String meal) {
+    // empty body
+  }
+}
